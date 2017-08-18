df54304b16ebc2bfe3f6ba1820a46004616c03f1
Maria <magsend@gmail.com>
2017-08-18 15:39:11 +0300

    Added bulk_insert into ElasticHelper

--- ElasticSearch/ElasticHelper.py
+++ ElasticSearch/ElasticHelper.py
@@ -14,6 +14,9 @@ import DButils
 import time
 import re
 import operator
+import os
+from datetime import datetime
+from elasticsearch import helpers
 
 def main():
     args = parsingArguments()
@@ -23,6 +26,9 @@ def main():
     if (args.input):
         global INPUT
         INPUT = args.input
+    if (args.output):
+        global OUTPUT
+        OUTPUT = args.output
     if (args.host):
         global HOST
         HOST = args.host
@@ -34,6 +40,9 @@ def main():
         USER = args.user
         global PWD
         PWD = args.pwd
+    if (args.type):
+        global SIZE
+        SIZE = args.size
     if (args.user and args.pwd):
         es = Elasticsearch([HOST+':'+PORT], http_auth=(USER, PWD))
     else:
@@ -67,36 +76,66 @@ def main():
     # '''
     # es.indices.clear_cache(index='mc16')
     start = time.time()
-
-    # res = es.search(index='mc16',
-    #           doc_type='event_summary',
-    #           body=query_body)
-    indexing(es_instance=es,
-             index_name='mc16',
-             doc_type='event_summary',
-             sql_file='../SQLRequests/mc16_campaign_without_category.sql',
-             remove_old=True,
-             mapping_file=None,
-             keyfield='taskid')
+    bulk_insert(es, 'prodsys', 'campaign', '../OracleProdSys2/mc16_campaign_for_ES.sql')
     end = time.time()
     print(end - start)
     # pprint.pprint(res)
     # print es.cluster.health()
     # print es.cluster.state()
     #pprint.pprint(es.indices.get_mapping(index='mc16'))
-    # indexing(es_instance=es,
-    #          index_name='mc16',
-    #          doc_type='event_summary',
-    #          sql_file='../SQLRequests/mc16_campaign_without_category.sql',
-    #          mapping_file=None,
-    #          keyfield='taskid')
-    # indexing(es_instance=es,
-    #          index_name='mc16_datasets',
-    #          doc_type='datasets_summary',
-    #          sql_file='../SQLRequests/mc16_datasets.sql',
-    #          mapping_file=None,
-    #          keyfield='taskid')
 
+def bulk_insert(es_instance, index_name, doc_type, sql_file, pagesize=100):
+    create_index(index_name, es_instance)
+    conn, cursor = DButils.connectDEFT_DSN(dsn)
+    handler = open(sql_file)
+    result = DButils.ResultIter(conn, handler.read()[:-1], pagesize, True)
+    actions = []
+    for row in result:
+        row["phys_category"] = get_category(row)
+        print row
+        actions.append(
+            {
+                "_index": index_name,
+                "_type": doc_type,
+                "_id": row.get('taskid'),
+                "_source": row,
+            })
+    helpers.bulk(es_instance, actions)
+
+
+def oracle2json(sql_file, output, arraysize=500):
+    conn, cursor = DButils.connectDEFT_DSN(dsn)
+
+    sql_handler = open(sql_file)
+    result = DButils.OneByOneIter(conn, sql_handler.read()[:-1], True)
+
+    counter = -1
+    if not os.path.exists(output):
+        os.makedirs(output)
+    json_handler = open('%s/%s_%d.json' % (output, output, 0), 'wb')
+    result_arr = []
+
+    for i in result:
+        i["phys_category"] = get_category(i.get("hashtag_list"), i.get("taskname"))
+        json_body = json.dumps(i, ensure_ascii=False)
+
+        if (counter%arraysize == 0):
+            json_handler = open('%s_%d.json' % (output, counter), 'wb')
+            json_handler.write('[')
+        result_arr.append(json_body)
+        json_handler.write(json_body)
+        counter += 1
+        if (counter % arraysize != 0):
+            json_handler.write(',')
+            json_handler.write('\n')
+        else:
+            json_handler.write(']')
+            json_handler.close()
+    if not json_handler.closed:
+        json_handler.seek(-1, os.SEEK_END)
+        json_handler.truncate()
+        json_handler.write(']')
+        json_handler.close()
 
 def indexing(es_instance, index_name, doc_type, sql_file, remove_old=True, mapping_file=None, keyfield=None):
     """
@@ -135,9 +174,10 @@ def indexing(es_instance, index_name, doc_type, sql_file, remove_old=True, mappi
     # set current timestamp
     curr_tstamp = datetime.datetime.now()
     id_counter = 0
-    file_handle = open("temp.txt", 'w')
+    # file_handle = open("temp.txt", 'w')
 
     for i in result:
+        print i
         i["phys_category"] = get_category(i.get("hashtag_list"), i.get("taskname"))
         # file_handle.write(i.get("hashtag_list")+"\n")
         # file_handle.write(i.get("taskname")+"\n")
@@ -156,9 +196,18 @@ def indexing(es_instance, index_name, doc_type, sql_file, remove_old=True, mappi
         except ElasticsearchException as e:
             print json_body
             print e
-    file_handle.close()
+    # file_handle.close()
 
-def get_category(hashtags, taskname, single=True):
+def get_category(row):
+    """
+    Each task can be associated with a number of Physics Categories.
+    1) search category in hashtags list
+    2) if not found in hashtags, then search category in phys_short field of tasknames
+    :param row
+    :return:
+    """
+    hashtags = row.get('hashtag_list')
+    taskname = row.get('taskname')
     PHYS_CATEGORIES_MAP = {'BPhysics':['charmonium','jpsi','bs','bd','bminus','bplus','charm','bottom','bottomonium','b0'],
                             'BTag':['btagging'],
                             'Diboson':['diboson','zz', 'ww', 'wz', 'wwbb', 'wwll'],
@@ -188,9 +237,6 @@ def get_category(hashtags, taskname, single=True):
     categories = [cat for cat in match if match[cat] > 0]
     if len(categories) > 0:
         return categories
-    #closest_category = max(match, key=match.get)
-    #if match[closest_category] != 0:
-    #    return closest_category
     else:
         phys_short = taskname.split('.')[2].lower()
         if re.search('singletop', phys_short) is not None: categories.append("SingleTop")
@@ -209,21 +255,6 @@ def get_category(hashtags, taskname, single=True):
         if re.search('_wwbb', phys_short) is not None: categories.append("SingleTop")
         if re.search('_wenu_', phys_short) is not None: categories.append("Wjets")
         return categories
-        # if re.search('singletop', phys_short) is not None: return "SingleTop"
-        # if re.search('ttbar',  phys_short) is not None: return "TTbar"
-        # if re.search('jets', phys_short) is not None: return "Multijet"
-        # if re.search('h125', phys_short) is not None: return "Higgs"
-        # if re.search('ttbb', phys_short) is not None: return "TTbarX"
-        # if re.search('ttgamma', phys_short) is not None: return "TTbarX"
-        # if re.search('_tt_', phys_short) is not None: return "TTbar"
-        # if re.search('upsilon', phys_short) is not None: return "BPhysics"
-        # if re.search('tanb', phys_short) is not None: return "SUSY"
-        # if re.search('4topci', phys_short) is not None: return "Exotic"
-        # if re.search('xhh', phys_short) is not None: return "Higgs"
-        # if re.search('3top', phys_short) is not None: return "TTbarX"
-        # if re.search('_wt', phys_short) is not None: return "SingleTop"
-        # if re.search('_wwbb', phys_short) is not None: return "SingleTop"
-        # if re.search('_wenu_', phys_short) is not None: return "Wjets"
     return "Uncategorized"
 
 
@@ -256,6 +287,8 @@ def parsingArguments():
     parser.add_argument('--pwd', help='ElasticSearch password')
     parser.add_argument('--type', help='type of input data: (file(f) | dir(d) | stream(s) | url(u))')
     parser.add_argument('--input', help='input data')
+    parser.add_argument('--output', help='Output directory')
+    parser.add_argument('--size', help='number of lines, processed at a time')
     # parser.add_argument('--o', help='output directory')
     return parser.parse_args()
 
@@ -318,6 +351,12 @@ def index_monitor_summary(index, doc_type, es):
 # def index_json_file(json_file, index, doc_type, es):
 #
 
+def createIndex(index_name, es):
+    try:
+        es.indices.create(index=index_name)
+    except ElasticsearchException.IndexAlreadyExistsException as e:
+        es.indices.delete(index=index_name)
+        es.indices.create(index=index_name)
 
 def removeIndex(index, es):
     try:
